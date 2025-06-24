38afe0622a7afff27a73ec6760b5f152f55aac2c
philips <philipsukonu@gmail.com>
2025-06-24 21:17:21 +0100

    update addons.tf

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,9 +1,10 @@
 provider "helm" {
-  "kubernetes" {
-    host                   = aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-    }
+  kubernetes {
+    # Connect to your EKS cluster
+    host                   = data.aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
 }
 
 provider "kubernetes" {
