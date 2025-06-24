78693a84255e23bf958a20a8e7c12c78fc1552e0
philips <philipsukonu@gmail.com>
2025-06-24 22:28:23 +0100

    update addons.tf

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,22 +1,24 @@
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.main.token
+provider "helm" {
+    kubernetes {
+        host                   = aws_eks_cluster.eks.endpoint
+        cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+        token                  = data.aws_eks_cluster_auth.eks.token
+    }
 }
 
-provider "helm" {
-  kubernetes =
-    host                   = data.aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.main.token
+provider "kubernetes" {
+  alias                  = "eks"
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 data "aws_eks_cluster_auth" "eks" {
-   name = aws_eks_cluster.eks.name
+    name = aws_eks_cluster.eks.name
 }
 resource "helm_release" "nginx_ingress" {
-   name       = "nginx-ingress"
-   repository = "https://kubernetes.github.io/ingress-nginx"
+    name       = "nginx-ingress"
+    repository = "https://kubernetes.github.io/ingress-nginx"
     chart      = "ingress-nginx"
     version    = "4.12.0"
     namespace  = "ingress-nginx"
@@ -50,12 +52,12 @@ resource "helm_release" "cert_manager" {
 #==================================================
 
 resource "helm_release" "argocd" {
-  name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  version          = "5.51.6"
-  namespace        = "argocd"
-  create_namespace = true
-  values           = [file("${path.module}/argocd-values.yaml")]
-  depends_on       = [helm_release.nginx_ingress, helm_release.cert_manager]
+    name             = "argocd"
+    repository       = "https://argoproj.github.io/argo-helm"
+    chart            = "argo-cd"
+    version          = "5.51.6"
+    namespace        = "argocd"
+    create_namespace = true
+    values = [file("${path.module}/argocd-values.yaml")]
+    depends_on = [ helm_release.nginx_ingress, helm_release.cert_manager]
 }
