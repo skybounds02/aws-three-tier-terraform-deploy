1dc561e2f3cda56d1ee6ecab7455f6261ca135b2
philips <philipsukonu@gmail.com>
2025-06-24 20:35:22 +0100

    backend.tf update

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "skybound02"
     key    = "s3-cicd/terraform.tfstate"
     region = "us-east-1"
-     dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
